6d6e2f0fbad0c61360c59b682d36f9946f8df61c
mzkmnk <sma0729mex@gmail.com>
2025-02-24 10:41:26 +0900

    feat: add auth library

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path:'auth',
+    loadChildren:() => import('@chat-style/auth').then((M) => M.authRoutes),
+  }
+];
